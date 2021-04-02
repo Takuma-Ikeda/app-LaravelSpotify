700a142ad601b692399399b8c03705654cb22abe
Takuma-Ikeda <el.programdear@gmail.com>
2021-04-02 13:50:42 +0900

    fix: resources/lang/ja/enums.php

--- resources/lang/ja/enums.php
+++ resources/lang/ja/enums.php
@@ -4,6 +4,9 @@ use App\Enums\Web;
 
 return[
     Web::class=> [
-        Web::GenreCreate => 'ジャンル生成',
+        Web::GenreCreate         => 'ジャンル作成',
+        Web::PlaylistStore       => 'プレイリスト作成',
+        Web::PlaylistDestroy     => 'プレイリスト削除',
+        Web::RecommendationStore => 'おすすめ作成',
     ],
 ];
