2a941d6345833e1c0055192482137b387f28775d
Takuma-Ikeda <el.programdear@gmail.com>
2021-04-02 13:34:02 +0900

    fix: エラー判定修正

--- resources/views/recomendation.blade.php
+++ resources/views/recomendation.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 <div class="container">
 
-@if ($result['error'])
+@if (isset($result['error']))
 <div class="row justify-content-center">
     <div class="col-md-8">
         <div class="card">
